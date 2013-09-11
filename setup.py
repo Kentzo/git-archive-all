170907e71dc469b52b63aefa065d55aff8f4dca9
David E. Wheeler <david@justatheory.com>
2013-09-11 10:31:28 -0700

    Improve setup.py.
    * Import setup from distutils.core if it can't be imported from setuptools.
    * Set the name in a variable.
    * Parse the version from the script.
    * Tweak the description.

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
-# you may need setuptools instead of distutils
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+SCRIPT="git-archive-all"
+
+# Parse the version from the file.
+verstrline = open(SCRIPT, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (SCRIPT,))
 
 setup(
-    name='git-archive-all',
-    version='1.7',
-    description='wrapper for git-archive that archives a git superproject and its submodules',
+    name=SCRIPT,
+    version=verstr,
+    description='Like git-archive, but archives a git superproject and its submodules',
     author='Ilya Kulakov',
     url='https://github.com/Kentzo/git-archive-all',
-    scripts = ['git-archive-all'],
+    scripts = [SCRIPT],
 )
