497049571f1cfe1c183cd3513b69914fa7379824
Ilya Kulakov <kulakov.ilya@gmail.com>
2015-10-25 16:33:11 -0400

    Remove unnecessary empty line in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Version Control',
-        'Topic :: System :: Archiving',
-
+        'Topic :: System :: Archiving'
     ]
 )
