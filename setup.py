00a229c9e65830857cbabb8a284755a3c2fc18cb
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-03-17 15:25:55 -0700

    Move data from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 import re
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup
 
 # Parse the version from the file.
 verstrline = open('git_archive_all.py', "rt").read()
@@ -15,29 +11,7 @@ else:
     raise RuntimeError("Unable to find version string in git_archive_all.py")
 
 setup(
-    name='git-archive-all',
     version=verstr,
-    description='Archive git repository with its submodules.',
-    author='Ilya Kulakov',
-    author_email="kulakov.ilya@gmail.com",
-    url='https://github.com/Kentzo/git-archive-all',
     py_modules=['git_archive_all'],
-    entry_points="""
-    [console_scripts]
-    git-archive-all=git_archive_all:main
-    """,
-    license="MIT License",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Version Control',
-        'Topic :: System :: Archiving'
-    ]
+    entry_points={'console_scripts': 'git-archive-all=git_archive_all:main'}
 )
